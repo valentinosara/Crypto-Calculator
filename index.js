8969fd7ba26fb655c4716805cf960c3f31ffe5ad
Valentino Sara <valentinosara2004@gmail.com>
2024-03-08 20:46:04 -0300

    created get-crypto-list function

--- index.js
+++ index.js
@@ -12,26 +12,34 @@ let investmentDate;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", async (req, res) => {
+async function getCryptoList() {
     const listParams = {
-        params : {
-            vs_currency : "usd",
-            order : "market_cap_desc",
-            page : 1,
-            per_page : 100,
-            sparkline : false,
-            price_change_percentage : "1h,24h,7d",
-            locale : "en"
-        }
+      params: {
+        vs_currency: "usd",
+        order: "market_cap_desc",
+        page: 1,
+        per_page: 100,
+        sparkline: false,
+        price_change_percentage: "1h,24h,7d",
+        locale: "en",
+      },
     };
+
     try {
-        const response =  await axios.get(API_URL + "/coins/markets", listParams);
-        const historyData = await axios.get(API_URL + "/coins/bitcoin/history?date=30-12-2022");
-        const cryptoArray = response.data;
+        const response = await axios.get(API_URL + "/coins/markets", listParams);
+        return response.data;
+      } catch (error) {
+        console.error("Failed to get crypto list:", error.message);
+        throw error;
+      }
+}
 
-        console.log(historyData.data.market_data.current_price.usd, selectedCoin);
-
-        res.render("index.ejs", {cryptoArray : cryptoArray});
+app.get("/", async (req, res) => {
+    try {
+        const cryptoArray = await getCryptoList();
+        // const historyData = await axios.get(API_URL + "/coins/bitcoin/history?date=30-12-2022");
+        console.log(selectedCoin);
+        res.render("index.ejs", {cryptoArray});
     } catch (error) {
         console.error("Failed to make request:", error.message);
         res.render("index.ejs", {
